d730583da04328fdad74c9a85367dff9e5ced3c8
alegrance <alegrance1000@gmail.com>
2023-05-02 22:31:00 -0400

    404 pokemon

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,20 @@
 module.exports = app => {
+    const htmlBody = `
+    <div style="text-align: center;">
+    <h1>ERROR 404</h1>
+    <br>
+    <img src="http://i.stack.imgur.com/SBv4T.gif" alt="I choose you!"  width="250" />
+    <br>
+    <h1>Page not found</h1>
+    </div>
+    `;
+
     app.get('/', (req, res) => {
-        res.json({status: 'API'})
-    })
+        res.status(404).send(htmlBody);
+        //res.json({status: 'API SimpleServer'})
+    });
+
+    app.route("/api").get((req, res) => {
+        res.status(404).send(htmlBody);
+      });
 };
